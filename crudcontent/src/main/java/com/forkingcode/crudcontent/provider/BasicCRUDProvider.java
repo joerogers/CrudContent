8230b1613095d5fecd55e9ee4e59f0f856daa442
Joe Rogers <smilinjoe87@gmail.com>
2015-10-21 16:27:06 -0700

    Improve error handling, add optional logging, explain bulk insert error logic

--- crudcontent/src/main/java/com/forkingcode/crudcontent/provider/BasicCRUDProvider.java
+++ crudcontent/src/main/java/com/forkingcode/crudcontent/provider/BasicCRUDProvider.java
@@ -24,34 +24,51 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteConstraintException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.BaseColumns;
+import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
+import android.util.Log;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Basic CRUD (Create, Read, Update, Delete) provider for a single table. Assumes basic
  * matching of either all rows, or by id.
- *
+ * <p/>
+ * The provider uses transactions for insert, update and deletes to provide optimal performance
+ * and the ability to cleanly implement rollbacks. It will use the proper type of transaction
+ * depending on if write ahead logging is enabled on the database.
+ * <p/>
  * URI for matching rows should be in form: content://{authority}/{table}
  * URI for matching row by id should be in form: content://{authority}/{table}/{id}
- *
+ * <p/>
  * By default the getType() method will return the following for a directory:
  * vnd.android.cursor.dir/{authority}/table
  * and the following for an individual row:
  * vnd.android.cursor.item/{authority}/table
- *
+ * <p/>
  * The following query parameters are supported on the URI:
  * distinct=true  - informs the query to ensure each row returned is unique.
  * limit={n} - return only the first "n" rows of data
+ *
+ * Note: if any errors occur in bulkInsert, update, or delete the provider will return -1
+ * to indicate an error occurred. 0 will be returned if the operation had no effect, but
+ * no error occurs.  If logging is enabled, more data on the errors will be recorded.
  */
 public abstract class BasicCRUDProvider extends ContentProvider {
+
+    private static final String TAG = "BasicCRUDProvider";
 
     public static final String DISTINCT_PARAMETER = "distinct";
     public static final String LIMIT_PARAMETER = "limit";
@@ -65,8 +82,47 @@ public abstract class BasicCRUDProvider extends ContentProvider {
 
     private static final String WHERE_MATCHES_ID = BaseColumns._ID + " = ?";
 
+
+    private static boolean LOGGING_ENABLED = false;
+
     private SQLiteOpenHelper dbHelper;
     private final String authority;
+
+    /**
+     * CONFLICT_ROLLBACK - all inserts (bulk or single), or updates will be rolled back on any
+     * data conflict or unexpected error that occurs with the sql command.
+     * <p/>
+     * Essentially behaves like CONFLICT_ROLLBACK, but uses CONFLICT_NONE (aka CONFLICT_ABORT) to
+     * detect errors, but this implementation of the provider will always rollback the transaction.
+     *
+     * @see SQLiteDatabase#CONFLICT_ROLLBACK
+     * @see SQLiteDatabase#CONFLICT_ABORT
+     */
+    protected static final int CONFLICT_ROLLBACK = SQLiteDatabase.CONFLICT_NONE;
+
+    /**
+     * CONFLICT_IGNORE - all inserts (bulk or single), or updates that result in a data conflict
+     * will be ignored leaving the original values in place, but the command will be successful.
+     *
+     * @see SQLiteDatabase#CONFLICT_IGNORE
+     */
+    protected static final int CONFLICT_IGNORE = SQLiteDatabase.CONFLICT_IGNORE;
+
+    /**
+     * CONFLICT_REPLACE - all inserts (bulk or single), that result in a data conflict
+     * will be removed, and the new row will be inserted in its place.
+     * <p/>
+     * For updates that result in conflict, the data resulting in the conflict will be removed
+     * prior to the update for the row completing.
+     *
+     * @see SQLiteDatabase#CONFLICT_REPLACE
+     */
+    protected static final int CONFLICT_REPLACE = SQLiteDatabase.CONFLICT_REPLACE;
+
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({CONFLICT_ROLLBACK, CONFLICT_IGNORE, CONFLICT_REPLACE})
+    public @interface ConflictAlgorithm {
+    }
 
     /**
      * Creates a BasicCRUDProvider.  Invoked by your subclass's constructor.
@@ -79,13 +135,18 @@ public abstract class BasicCRUDProvider extends ContentProvider {
         uriMatcher.addURI(authority, "*/#", ROW_BY_ID);
     }
 
+    /**
+     * Enable diagnostic logging. Best if flag is tied to BuildConfig.DEBUG for the application
+     *
+     * @param enabled true if diagnostic logging should occur
+     */
+    public static void setLoggingEnabled(boolean enabled) {
+        LOGGING_ENABLED = enabled;
+    }
+
     @Override
     public boolean onCreate() {
         dbHelper = getDbHelper();
-        Context context = getContext();
-        if (context != null) {
-            context.registerComponentCallbacks(this);
-        }
         return true;
     }
 
@@ -94,10 +155,6 @@ public abstract class BasicCRUDProvider extends ContentProvider {
      */
     @Override
     public final void shutdown() {
-        Context context = getContext();
-        if (context != null) {
-            context.unregisterComponentCallbacks(this);
-        }
         dbHelper.close();
         dbHelper = null;
     }
@@ -107,6 +164,7 @@ public abstract class BasicCRUDProvider extends ContentProvider {
      *
      * @return The SQLiteOpen helper used to access the database
      */
+    @NonNull
     protected abstract SQLiteOpenHelper getDbHelper();
 
     /**
@@ -117,26 +175,45 @@ public abstract class BasicCRUDProvider extends ContentProvider {
      * Default is null
      */
     @SuppressWarnings({"SameReturnValue", "UnusedParameters", "WeakerAccess"})
+    @Nullable
     protected String getNullColumnHack(@NonNull String table) {
         return null;
     }
 
     /**
      * Override to provide a insert conflict algorithm for the specified table.
-     *
-     * By default it will use SQLiteDatabase.CONFLICT_NONE which is equivalent to
-     * CONFLICT_ABORT per SQLite specification
+     * <p/>
+     * By default it will use CONFLICT_ROLLBACK which will roll back the insert
+     * operation if a conflict occurs leaving the database unchanged.
      *
      * @param table The table to determine the conflict algorithm for.
      * @return SQLiteDatabase conflict algorithm
      * @see SQLiteDatabase
      */
     @SuppressWarnings("UnusedParameters")
-    protected int getConflictAlgorithm(@NonNull String table) {
-        return SQLiteDatabase.CONFLICT_NONE;
+    @ConflictAlgorithm
+    protected int getInsertConflictAlgorithm(@NonNull String table) {
+        return CONFLICT_ROLLBACK;
+    }
+
+    /**
+     * Override to provide an update conflict algorithm for the specified table.
+     * <p/>
+     * By default it will use CONFLICT_ROLLBACK which will roll back the update
+     * operation if a conflict occurs leaving the database unchanged.
+     *
+     * @param table The table to determine the conflict algorithm for.
+     * @return SQLiteDatabase conflict algorithm
+     * @see SQLiteDatabase
+     */
+    @SuppressWarnings("UnusedParameters")
+    @ConflictAlgorithm
+    protected int getUpdateConflictAlgorithm(@NonNull String table) {
+        return CONFLICT_ROLLBACK;
     }
 
     @Override
+    @Nullable
     public String getType(@NonNull Uri uri) {
         int match = uriMatcher.match(uri);
         String table;
@@ -153,7 +230,8 @@ public abstract class BasicCRUDProvider extends ContentProvider {
     }
 
     @Override
-    public Uri insert(@NonNull Uri uri, ContentValues values) {
+    @Nullable
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
         int match = uriMatcher.match(uri);
         String table;
         switch (match) {
@@ -171,10 +249,18 @@ public abstract class BasicCRUDProvider extends ContentProvider {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         db.acquireReference();
         try {
+            int conflictAlgorithm = translateConflictAlgorithm(getInsertConflictAlgorithm(table));
             startTransaction(db);
             try {
-                id = db.insertWithOnConflict(table, getNullColumnHack(table), values, getConflictAlgorithm(table));
+                id = db.insertWithOnConflict(table, getNullColumnHack(table), values, conflictAlgorithm);
                 db.setTransactionSuccessful();
+            }
+            catch (SQLiteException e) {
+                if (LOGGING_ENABLED) {
+                    Log.e(TAG, "Error inserting " + table +
+                            " with " + values, e);
+                }
+                id = -1;
             }
             finally {
                 db.endTransaction();
@@ -216,15 +302,32 @@ public abstract class BasicCRUDProvider extends ContentProvider {
             startTransaction(db);
             try {
                 final String nullColumnHack = getNullColumnHack(table);
-                final int conflictAlgorithm = getConflictAlgorithm(table);
+                final int conflictAlgorithm = translateConflictAlgorithm(getInsertConflictAlgorithm(table));
 
                 for (ContentValues values : valuesArray) {
-                    long id = db.insertWithOnConflict(table, nullColumnHack, values, conflictAlgorithm);
+                    long id;
+                    try {
+                        id = db.insertWithOnConflict(table, nullColumnHack, values, conflictAlgorithm);
+                    }
+                    catch (SQLiteConstraintException e) {
+                        if (LOGGING_ENABLED) {
+                            Log.e(TAG, "Error inserting " + table +
+                                    " with " + values, e);
+                        }
+                        throw e;
+                    }
+
                     if (id != -1) {
                         ++count;
                     }
                 }
                 db.setTransactionSuccessful();
+            }
+            catch (SQLiteException e) {
+                if (LOGGING_ENABLED && !(e instanceof SQLiteConstraintException)) {
+                    Log.e(TAG, "Unexpected error bulk inserting " + table, e);
+                }
+                count = -1;
             }
             finally {
                 db.endTransaction();
@@ -243,8 +346,13 @@ public abstract class BasicCRUDProvider extends ContentProvider {
     }
 
     @Override
-    public Cursor query(@NonNull Uri uri, String[] projection, String selection,
-                        String[] selectionArgs, String sortOrder) {
+    @Nullable
+    public Cursor query(@NonNull Uri uri,
+                        @Nullable String[] projection,
+                        @Nullable String selection,
+                        @Nullable String[] selectionArgs,
+                        @Nullable String sortOrder) {
+
         int match = uriMatcher.match(uri);
         String table;
         String useSelection = selection;
@@ -286,14 +394,24 @@ public abstract class BasicCRUDProvider extends ContentProvider {
             }
             return cursor;
         }
+        catch (SQLiteException e) {
+            if (LOGGING_ENABLED) {
+                Log.e(TAG, "Error querying " + table +
+                        " where " + useSelection + " " + Arrays.toString(useSelectionArgs), e);
+            }
+            return null;
+        }
         finally {
             db.releaseReference();
         }
     }
 
     @Override
-    public int update(@NonNull Uri uri, ContentValues values, String selection,
-                      String[] selectionArgs) {
+    public int update(@NonNull Uri uri,
+                      @Nullable ContentValues values,
+                      @Nullable String selection,
+                      @Nullable String[] selectionArgs) {
+
         int match = uriMatcher.match(uri);
         String table;
         String useSelection = selection;
@@ -320,8 +438,16 @@ public abstract class BasicCRUDProvider extends ContentProvider {
         try {
             startTransaction(db);
             try {
-                rows = db.update(table, values, useSelection, useSelectionArgs);
+                final int conflictAlgorithm = translateConflictAlgorithm(getUpdateConflictAlgorithm(table));
+                rows = db.updateWithOnConflict(table, values, useSelection, useSelectionArgs, conflictAlgorithm);
                 db.setTransactionSuccessful();
+            }
+            catch (SQLiteException e) {
+                if (LOGGING_ENABLED) {
+                    Log.e(TAG, "Error updating " + table +
+                            " where " + useSelection + " " + Arrays.toString(useSelectionArgs) +
+                            " with " + values, e);
+                }
             }
             finally {
                 db.endTransaction();
@@ -340,7 +466,10 @@ public abstract class BasicCRUDProvider extends ContentProvider {
     }
 
     @Override
-    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+    public int delete(@NonNull Uri uri,
+                      @Nullable String selection,
+                      @Nullable String[] selectionArgs) {
+
         int match = uriMatcher.match(uri);
         String table;
         String useSelection = selection;
@@ -368,6 +497,12 @@ public abstract class BasicCRUDProvider extends ContentProvider {
                 rows = db.delete(table, useSelection, useSelectionArgs);
                 db.setTransactionSuccessful();
             }
+            catch (SQLiteException e) {
+                if (LOGGING_ENABLED) {
+                    Log.e(TAG, "Error deleting " + table +
+                            " where " + useSelection + " " + Arrays.toString(useSelectionArgs), e);
+                }
+            }
             finally {
                 db.endTransaction();
             }
@@ -390,7 +525,8 @@ public abstract class BasicCRUDProvider extends ContentProvider {
         if (level >= ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN) {
             // In the background. Close the database via the helper.
             // If there is an active connection, it will continue to process
-            // due to reference counting, and close when all references are released.
+            // due to explicit reference counting added via this provider implementation.
+            // The database will close when all references are released.
             dbHelper.close();
         }
     }
@@ -414,6 +550,20 @@ public abstract class BasicCRUDProvider extends ContentProvider {
         }
         else {
             db.beginTransaction();
+        }
+    }
+
+    private static int translateConflictAlgorithm(int algorithm) {
+        switch (algorithm) {
+            case CONFLICT_ROLLBACK:
+            case CONFLICT_IGNORE:
+            case CONFLICT_REPLACE:
+                return algorithm;
+            default:
+                if (LOGGING_ENABLED) {
+                    Log.w(TAG, "Unexpected conflict algorithm provided: " + algorithm);
+                }
+                return CONFLICT_REPLACE;
         }
     }
 }
