1934ef6b68e100053819c5c17031ecb1af829331
Joe Rogers <joerogers@users.noreply.github.com>
2016-07-18 21:11:28 -0700

    Add new insert task

---
+++ crudcontent/src/main/java/com/forkingcode/crudcontent/task/BasicCRUDInsertTask.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2016 Joe Rogers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.forkingcode.crudcontent.task;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v4.content.LocalBroadcastManager;
+
+/**
+ * An async task for inserting data asynchronously. The task uses the AsyncTask tread pool allowing
+ * multiple tasks to operate concurrently.
+ */
+public class BasicCRUDInsertTask extends AsyncTask<BasicCRUDInsertTask.Builder, Void, Void> {
+
+    public static final String INSERT_COMPLETE_ACTION = "com.forkingcode.crudcontent.action.insert_complete";
+    public static final String EXTRA_URI = "com.forkingcode.crudcontent.extra.uri";
+    public static final String EXTRA_ROWS = "com.forkingcode.crudcontent.extra.rows";
+
+    /**
+     * Builder uses to create a new insert task
+     */
+    public static class Builder {
+        private final Context applicationContext;
+        private Uri uri;
+        private ContentValues[] valuesArray;
+        private boolean resultBroadcastRequested = false;
+
+        /**
+         * Create a new insert task builder
+         *
+         * @param context A context used in the creation of the task. The application context
+         *                will be retrieved via this context, to avoid holding direct
+         *                references to activities, etc.
+         */
+        public Builder(@NonNull Context context) {
+            this.applicationContext = context.getApplicationContext();
+        }
+
+        /**
+         * Provide the Uri for insertion. You must call this method on the builder. Failure to do so
+         * will result in an IllegalStateException when start() is called.
+         *
+         * @param uri The uri associated with the provider to query.
+         * @return This builder object
+         */
+        @NonNull
+        public Builder forUri(Uri uri) {
+            this.uri = uri;
+            return this;
+        }
+
+        /**
+         * Provide a single row of values for the insert operation.
+         *
+         * @param values The content values indicating the columns/value pairs for the operation
+         * @return this intent builder
+         */
+        @NonNull
+        public Builder usingValues(@NonNull ContentValues values) {
+            valuesArray = new ContentValues[]{values};
+            return this;
+        }
+
+        /**
+         * Provide multiple rows of values for the bulk insert operation via an array
+         *
+         * @param values The content values array indicating the columns/value pairs for the operation
+         * @return this intent builder
+         */
+        public Builder usingValues(@NonNull ContentValues[] values) {
+            valuesArray = values;
+            return this;
+        }
+
+        /**
+         * Request the task to send an local broadcast with the result of the insert operation using
+         * the INSERT_COMPLETE_ACTION, otherwise no broadcast is sent.
+         * If one row was provided, the uri representing the inserted row will be provided via EXTRA_URI.
+         * The number of rows inserted will be provided via EXTRA_ROWS.
+         *
+         * @return this intent builder
+         */
+        public Builder requestResultBroadcast() {
+            resultBroadcastRequested = true;
+            return this;
+        }
+
+        /**
+         * Start the insert task.
+         *
+         * @return An instance of the BasicCRUDInsertTask that could be used for cancellation
+         * @throws IllegalStateException if uri or on or more ContentValues was not provided
+         */
+        public BasicCRUDInsertTask start() {
+            if (uri == null) {
+                throw new IllegalStateException("Must provide URI");
+            }
+            if (valuesArray == null) {
+                throw new IllegalStateException("Must provide content values");
+            }
+
+            BasicCRUDInsertTask task = new BasicCRUDInsertTask();
+            task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, this);
+            return task;
+        }
+    }
+
+    private BasicCRUDInsertTask() {
+    }
+
+    @Override
+    protected Void doInBackground(Builder... builders) {
+        Builder builder = builders[0];
+
+        Uri uri = null;
+        int rows;
+
+        if (isCancelled()) {
+            return null;
+        }
+
+        if (builder.valuesArray.length == 1) {
+            uri = builder.applicationContext.getContentResolver()
+                    .insert(builder.uri, builder.valuesArray[0]);
+            rows = uri != null ? 1 : 0;
+        }
+        else {
+            rows = builder.applicationContext.getContentResolver()
+                    .bulkInsert(builder.uri, builder.valuesArray);
+        }
+
+        if (builder.resultBroadcastRequested && !isCancelled()) {
+            Intent resultIntent = new Intent(INSERT_COMPLETE_ACTION);
+            resultIntent.putExtra(EXTRA_ROWS, rows);
+            if (uri != null) {
+                resultIntent.putExtra(EXTRA_URI, uri);
+            }
+
+            LocalBroadcastManager.getInstance(builder.applicationContext)
+                    .sendBroadcast(resultIntent);
+        }
+
+        return null;
+    }
+}
---
+++ testapp/src/androidTest/java/com/example/crudtester/task/BasicCRUDInsertTaskTest.java
@@ -0,0 +1,240 @@
+/*
+ * Copyright 2016 Joe Rogers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.crudtester.task;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.content.LocalBroadcastManager;
+
+import com.forkingcode.crudcontent.task.BasicCRUDInsertTask;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import java.util.concurrent.CancellationException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Test insertion via the BasicCRUDInsertTask
+ */
+@SmallTest
+@RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class BasicCRUDInsertTaskTest {
+
+
+    private static final IntentFilter INTENT_FILTER = new IntentFilter(BasicCRUDInsertTask.INSERT_COMPLETE_ACTION);
+    private static final String AUTHORITY = "test";
+    private static final String TABLE = "my_table";
+    private static final Uri URI = new Uri.Builder()
+            .scheme(ContentResolver.SCHEME_CONTENT)
+            .authority(AUTHORITY)
+            .appendPath(TABLE)
+            .build();
+
+    private Context context;
+    private InsertBroadcastReceiver receiver;
+
+    @Before
+    public void setup() {
+        context = new TaskMockContext(AUTHORITY, InstrumentationRegistry.getTargetContext());
+        receiver = new InsertBroadcastReceiver();
+        LocalBroadcastManager.getInstance(context).registerReceiver(receiver, INTENT_FILTER);
+    }
+
+    @After
+    public void tearDown() {
+        LocalBroadcastManager.getInstance(context).unregisterReceiver(receiver);
+        receiver = null;
+        context = null;
+    }
+
+    @Test
+    public void test01Insert() throws Exception {
+
+        assertNull("Intent not null", receiver.intent);
+
+        BasicCRUDInsertTask task = new BasicCRUDInsertTask.Builder(context)
+                .forUri(URI)
+                .usingValues(new ContentValues())
+                .requestResultBroadcast()
+                .start();
+
+        task.get();
+        Thread.sleep(5);
+        assertNotNull("Intent null", receiver.intent);
+        assertEquals(BasicCRUDInsertTask.INSERT_COMPLETE_ACTION, receiver.intent.getAction());
+
+        Uri uri = receiver.intent.getParcelableExtra(BasicCRUDInsertTask.EXTRA_URI);
+        assertNotNull("Uri null", uri);
+        long id = ContentUris.parseId(uri);
+        assertEquals("Invalid id", TaskMockContentProvider.INSERT_ID_RESULT, id);
+
+        int rows = receiver.intent.getIntExtra(BasicCRUDInsertTask.EXTRA_ROWS, 0);
+        assertEquals("Incorrect rows", 1, rows);
+    }
+
+    @Test
+    public void test02BulkInsert() throws Exception {
+
+        assertNull("Intent not null", receiver.intent);
+
+        ContentValues[] valuesArray = new ContentValues[2];
+        valuesArray[0] = new ContentValues();
+        valuesArray[1] = new ContentValues();
+
+        BasicCRUDInsertTask task = new BasicCRUDInsertTask.Builder(context)
+                .forUri(URI)
+                .usingValues(valuesArray)
+                .requestResultBroadcast()
+                .start();
+
+        task.get();
+        Thread.sleep(5);
+        assertNotNull("Intent null", receiver.intent);
+        assertEquals(BasicCRUDInsertTask.INSERT_COMPLETE_ACTION, receiver.intent.getAction());
+
+        Uri uri = receiver.intent.getParcelableExtra(BasicCRUDInsertTask.EXTRA_URI);
+        assertNull("Uri not null", uri);
+
+        int rows = receiver.intent.getIntExtra(BasicCRUDInsertTask.EXTRA_ROWS, 0);
+        assertEquals("Incorrect rows", valuesArray.length, rows);
+    }
+
+    @Test
+    public void test03InsertCancelled() throws Exception {
+        assertNull("Intent not null", receiver.intent);
+
+        BasicCRUDInsertTask task = new BasicCRUDInsertTask.Builder(context)
+                .forUri(URI)
+                .usingValues(new ContentValues())
+                .start();
+
+        boolean cancelled = false;
+        try {
+            task.cancel(true);
+            task.get();
+        }
+        catch (CancellationException e) {
+            cancelled = true;
+        }
+
+        assertNull("Intent not null", receiver.intent);
+        assertTrue("Task not cancelled", cancelled);
+    }
+
+    @Test
+    public void test04InsertWithBroadcastCancelled() throws Exception {
+        assertNull("Intent not null", receiver.intent);
+
+        BasicCRUDInsertTask task = new BasicCRUDInsertTask.Builder(context)
+                .forUri(URI)
+                .usingValues(new ContentValues())
+                .requestResultBroadcast()
+                .start();
+
+        boolean cancelled = false;
+        try {
+            task.cancel(true);
+            task.get();
+        }
+        catch (CancellationException e) {
+            cancelled = true;
+        }
+
+        assertNull("Intent not null", receiver.intent);
+        assertTrue("Task not cancelled", cancelled);
+    }
+
+    @Test
+    public void test05InsertNoBroadcast() throws Exception {
+        assertNull("Intent not null", receiver.intent);
+
+        BasicCRUDInsertTask task = new BasicCRUDInsertTask.Builder(context)
+                .forUri(URI)
+                .usingValues(new ContentValues())
+                .start();
+
+        task.get();
+
+        assertNull("Intent not null", receiver.intent);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test06NoContentValuesForInsert() throws Exception {
+
+        BasicCRUDInsertTask task = new BasicCRUDInsertTask.Builder(context)
+                .forUri(URI)
+                .start();
+
+        assertNull("Task not null", task);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test07NoUriForInsert() throws Exception {
+
+        BasicCRUDInsertTask task = new BasicCRUDInsertTask.Builder(context)
+                .usingValues(new ContentValues())
+                .start();
+
+        assertNull("Task not null", task);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void test08NoContextForInsert() throws Exception {
+
+        // Suppressing warning for passing null validated by Android inspections.
+        @SuppressWarnings("ConstantConditions")
+        BasicCRUDInsertTask task = new BasicCRUDInsertTask.Builder(null)
+                .forUri(URI)
+                .usingValues(new ContentValues())
+                .start();
+
+        assertNull("Task not null", task);
+    }
+
+
+    /**
+     * Helper receiver to catch intent for validation
+     */
+    static class InsertBroadcastReceiver extends BroadcastReceiver {
+
+        public Intent intent = null;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            this.intent = intent;
+        }
+    }
+}
